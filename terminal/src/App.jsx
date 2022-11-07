2729f3993d30f98357cc1a7e756f28a585e1e7a2
Devansh Arora <dev.marvel.avengers@gmail.com>
2022-11-07 23:24:51 +0530

    commit

--- terminal/src/App.jsx
+++ terminal/src/App.jsx
@@ -8,11 +8,6 @@ function App() {
 				Press F11 for fullscreen
 			</div>
       <div className="parent">
-        {/*<img className="monitor" src={frame} alt="monitor-frames" />*/}
-        {/*<img className="frame"
-          src="https://www.pngkey.com/png/detail/71-719419_old-tv-frame-png-siteframes-co-display-device.png"
-          alt="Old Tv Frame Png Siteframes Co - Display Device@pngkey.com"
-        ></img>*/}
         <Terminal className="term" />
       </div>
     </>
