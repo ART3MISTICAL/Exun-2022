2f3a6bd2cfb01120e1bd00324933f2cbb9a8dd55
Devansh Arora <dev.marvel.avengers@gmail.com>
2022-11-07 15:22:37 +0530

    commit

--- terminal/src/App.js
+++ terminal/src/App.js
@@ -1,18 +1,12 @@
-import { ReactTerminal } from "react-terminal";
-import './terminal.module.css'
+import Terminal from './components/terminal'
 
 
-function App(props) {
-	const commands = {
-		play: "The story takes place in a utopia Ihleda with a dark secret:- The balance of the utopia's happiness Is caused by entrapping the Saint Maglena within a secret location of the headquarters. The story centresw around 17 yr old Anubis, who is tasked with setting Amglena free after he receivers a message from the saint within a dream.",
-	};
+function App() {
+
 	return (
-		<div className="terminal-space">
-			<ReactTerminal className="terminal"
-				commands={commands}
-				theme="dark"
-			/>
-		</div>
+		<>
+			<Terminal/>
+		</>
 	);
 }
 
