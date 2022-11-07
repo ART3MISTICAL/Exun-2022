328febefefc9916139e466bf8f39d06a024bb956
Devansh Arora <dev.marvel.avengers@gmail.com>
2022-11-07 17:15:51 +0530

    commit

--- terminal/src/App.jsx
+++ terminal/src/App.jsx
@@ -1,13 +1,19 @@
-import Terminal from './components/terminal'
-
+import Terminal from "./components/terminal";
+//import frame from "./monitor.png";
 
 function App() {
-
-	return (
-		<>
-			<Terminal/>
-		</>
-	);
+  return (
+    <>
+      <div className="parent">
+        {/*<img className="monitor" src={frame} alt="monitor-frames" />*/}
+        {/*<img className="frame"
+          src="https://www.pngkey.com/png/detail/71-719419_old-tv-frame-png-siteframes-co-display-device.png"
+          alt="Old Tv Frame Png Siteframes Co - Display Device@pngkey.com"
+        ></img>*/}
+        <Terminal className="term" />
+      </div>
+    </>
+  );
 }
 
 export default App;
