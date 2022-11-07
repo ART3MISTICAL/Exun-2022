275032803092fe99c0a028c972a7da7b6b6297a9
Devansh Arora <dev.marvel.avengers@gmail.com>
2022-11-07 20:28:20 +0530

    commit

--- terminal/src/components/terminal.jsx
+++ terminal/src/components/terminal.jsx
@@ -1,47 +1,59 @@
 import { ReactTerminal } from "react-terminal";
 import "../terminal.module.css";
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
 
 function App(props) {
-	const [index , setIndex] =	useState('start');
+  const [index, setIndex] = useState("start");
   //const commands = {
   //  play: "The story takes place in a utopia Ihleda with a dark secret:- The balance of the utopia's happiness Is caused by entrapping the Saint Maglena within a secret location of the headquarters. The story centresw around 17 yr old Anubis, who is tasked with setting Amglena free after he receivers a message from the saint within a dream.",
-    
+
   //};
 
   const array = {
     start: {
       play: () => {
-        setIndex(prevState => '1');
-        return "The story takes place in a utopia Ihleda with a dark secret:- The balance of the utopia's happiness Is caused by entrapping the Saint Maglena within a secret location of the headquarters. The story centresw around 17 yr old Anubis, who is tasked with setting Amglena free after he receivers a message from the saint within a dream."
-    }
-	
-			,
-
+        setIndex((prevState) => "1");
+        return [
+          "The story takes place in a utopia Ihleda with a dark secret and centres around 17 yr old Anubis, who is tasked with setting Amglena free after he receivers a message from the saint within a dream.",
+          <br />,
+          <br />,
+          "Anubis wakes up from a dream and recounts the prophecy he received in his dream: ",
+          // <br />,
+          <br />,
+          "He is meant to find Maglena and set him free from a place named Armageddon that he doesn't know the location of.",
+          <br />,
+          <br />,
+          "He is offered two choices:",
+          <br />,
+          <br />,
+          "a)Look for Armageddon in the Ihleda national library maps",
+          <br />,
+          <br />,
+          "b) Ask his father (the president of Ilheda) about Armageddon",
+        ];
+      },
     },
-		1: {
-			a: () =>{
-				setIndex(prevState => '1.1');
-				return 'a option'
-			},
-			b: () => {
-				setIndex(prevState => prevState + 2);
-				return 'b option'
-			},
-
-		},
-		1.1: {
-			a: () => {
-				setIndex(prevState => prevState + 1);
-				return 'a situation'
-			},
-			b: () => {
-				setIndex(prevState => prevState + 2);
-				return 'a sit'
-			}
-		}
-	};
+    1: {
+      a: () => {
+        setIndex((prevState) => "1.1");
+        return "a option";
+      },
+      b: () => {
+        setIndex((prevState) => prevState + 2);
+        return "b option";
+      },
+    },
+    1.1: {
+      a: () => {
+        setIndex((prevState) => prevState + 1);
+        return "a situation";
+      },
+      b: () => {
+        setIndex((prevState) => prevState + 2);
+        return "a sit";
+      },
+    },
+  };
 
   return (
     <div className="terminal-space">
